4e9933f1ed0e255df459d06cb7b71cd2c64babac
ScAxeliss <91017997+Axeliss@users.noreply.github.com>
2021-11-13 17:08:04 +0100

    [add] levels of enchantments

--- src/fr/scaxeliss/duelplugin/kit/GiveKits.java
+++ src/fr/scaxeliss/duelplugin/kit/GiveKits.java
@@ -15,27 +15,27 @@ public class GiveKits {
         player.getInventory().setHelmet(new ItemStack(Material.CHAINMAIL_HELMET));
         player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
         player.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
-        player.getInventory().setItem(0, ItemBuilder.itemWithEnchant(Material.BOW, "§fArc Infini", Enchantment.ARROW_INFINITE));
+        player.getInventory().setItem(0, ItemBuilder.itemWithEnchant(Material.BOW, "§fArc Infini", Enchantment.ARROW_INFINITE, 1));
         player.getInventory().setItem(6, new ItemStack(Material.ARROW));
         player.getInventory().setItem(1, new ItemStack(Material.WOOD, 64));
         player.getInventory().setItem(2, new ItemStack(Material.WOOD, 64));
         player.getInventory().setItem(2, new ItemStack(Material.GOLDEN_APPLE, 5));
-        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bûcheron Dimitri",Enchantment.DIG_SPEED ));
+        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bûcheron Dimitri",Enchantment.DIG_SPEED, 2));
         player.getInventory().setHeldItemSlot(0);
     }
 
     private static void giveKitWitch(Player player) {
         player.getInventory().clear();
-        player.getInventory().setChestplate(ItemBuilder.itemWithEnchant(Material.GOLD_CHESTPLATE, "§fPlastron en or P1", Enchantment.PROTECTION_ENVIRONMENTAL));
-        player.getInventory().setHelmet(ItemBuilder.itemWithEnchant(Material.GOLD_HELMET, "§fCasque en or P1", Enchantment.PROTECTION_ENVIRONMENTAL));
-        player.getInventory().setLeggings(ItemBuilder.itemWithEnchant(Material.GOLD_LEGGINGS, "§fJambières en or P1", Enchantment.PROTECTION_ENVIRONMENTAL));
-        player.getInventory().setBoots(ItemBuilder.itemWithEnchant(Material.GOLD_BOOTS, "§fBottes en or P1", Enchantment.PROTECTION_ENVIRONMENTAL));
+        player.getInventory().setChestplate(ItemBuilder.itemWithEnchant(Material.GOLD_CHESTPLATE, "§fPlastron en or P1", Enchantment.PROTECTION_ENVIRONMENTAL,1));
+        player.getInventory().setHelmet(ItemBuilder.itemWithEnchant(Material.GOLD_HELMET, "§fCasque en or P1", Enchantment.PROTECTION_ENVIRONMENTAL,1));
+        player.getInventory().setLeggings(ItemBuilder.itemWithEnchant(Material.GOLD_LEGGINGS, "§fJambières en or P1", Enchantment.PROTECTION_ENVIRONMENTAL,1));
+        player.getInventory().setBoots(ItemBuilder.itemWithEnchant(Material.GOLD_BOOTS, "§fBottes en or P1", Enchantment.PROTECTION_ENVIRONMENTAL,1));
         player.getInventory().setItem(1, new ItemStack(Material.WOOD, 64));
         player.getInventory().setItem(2, new ItemStack(Material.WOOD, 64));
         player.getInventory().setItem(3, new ItemStack(Material.GOLDEN_APPLE, 5));
-        player.getInventory().setItem(0, ItemBuilder.itemWithEnchant(Material.STONE_SWORD,"§fEpée en pierre" ,Enchantment.DAMAGE_ALL));
+        player.getInventory().setItem(0, ItemBuilder.itemWithEnchant(Material.STONE_SWORD,"§fEpée en pierre" ,Enchantment.DAMAGE_ALL, 1));
         player.getInventory().setItem(4, new ItemStack(Material.POTION, 10, (byte) 8229));
-        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bucheron Dimitri",Enchantment.DIG_SPEED));
+        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bucheron Dimitri",Enchantment.DIG_SPEED,1));
         player.getInventory().setHeldItemSlot(0);
     }
 
@@ -50,7 +50,7 @@ public class GiveKits {
         player.getInventory().setItem(2, new ItemStack(Material.GOLDEN_APPLE, 3));
         player.getInventory().setItem(0, new ItemStack(Material.IRON_SWORD ));
         player.getInventory().setItem(1, new ItemStack(Material.FISHING_ROD));
-        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bûcheron Dimitri",Enchantment.DIG_SPEED ));
+        player.getInventory().setItem(8, ItemBuilder.itemWithEnchant(Material.IRON_AXE, "§fHache du Bûcheron Dimitri",Enchantment.DIG_SPEED,1));
 
     }
 
