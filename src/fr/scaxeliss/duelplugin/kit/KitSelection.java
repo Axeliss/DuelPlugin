767c9939967ea9c601a6125f97fbdd371adc321b
ScAxeliss <91017997+Axeliss@users.noreply.github.com>
2021-12-01 12:44:10 +0100

    Update KitSelection.java

--- src/fr/scaxeliss/duelplugin/kit/KitSelection.java
+++ src/fr/scaxeliss/duelplugin/kit/KitSelection.java
@@ -1,5 +1,6 @@
 package fr.scaxeliss.duelplugin.kit;
 
+import fr.scaxeliss.duelplugin.Main;
 import fr.scaxeliss.duelplugin.managers.ItemBuilder;
 import fr.scaxeliss.duelplugin.scoreboard.LobbyScoreboard;
 import org.bukkit.Bukkit;
@@ -16,25 +17,25 @@ public class KitSelection {
     public static void kitInventory(Player player){
         Inventory kit = Bukkit.createInventory(null, 54, "§7Kits");
 
-        if(!Kits.kit.get(player).equalsIgnoreCase("archer")){
+        if(!Main.kit.get(player).equalsIgnoreCase("archer")){
             kit.setItem(0,ItemBuilder.itemWithLore(Material.BOW, "§aArcher", "", "§eClique pour sélectionner le kit Archer !"));
-        } else if(Kits.kit.get(player).equalsIgnoreCase("Archer")) {
+        } else if(Main.kit.get(player).equalsIgnoreCase("Archer")) {
             kit.setItem(0,ItemBuilder.itemWithEnchantAndLore(Material.BOW, "§aArcher", "","§eSELECTIONNE", Enchantment.DAMAGE_ALL));
         }
 
-        if(!Kits.kit.get(player).equalsIgnoreCase("guerrier")){
+        if(!Main.kit.get(player).equalsIgnoreCase("guerrier")){
             kit.setItem(1,ItemBuilder.itemWithLore(Material.IRON_SWORD, "§aGuerrier", "", "§eClique pour sélectionner le kit Guerrier !"));
-        } else if(Kits.kit.get(player).equalsIgnoreCase("Guerrier")){
+        } else if(Main.kit.get(player).equalsIgnoreCase("Guerrier")){
             kit.setItem(1,ItemBuilder.itemWithEnchantAndLore(Material.IRON_SWORD, "§aGuerrier", "","§eSELECTIONNE", Enchantment.DAMAGE_ALL));
         }
 
-        if(!Kits.kit.get(player).equalsIgnoreCase("sorcier")){
+        if(!Main.kit.get(player).equalsIgnoreCase("sorcier")){
             kit.setItem(2,ItemBuilder.itemWithLore(Material.POTION, "§aSorcier", "", "§eClique pour sélectionner le kit Sorcier !"));
-        } else if(Kits.kit.get(player).equalsIgnoreCase("sorcier")){
+        } else if(Main.kit.get(player).equalsIgnoreCase("sorcier")){
             kit.setItem(2,ItemBuilder.itemWithEnchantAndLore(Material.POTION, "§aSorcier", "","§eSELECTIONNE", Enchantment.DAMAGE_ALL));
         }
 
-        if(Kits.kit.get(player).equalsIgnoreCase("aucun")){
+        if(Main.kit.get(player).equalsIgnoreCase("aucun")){
 
         }
 
@@ -49,31 +50,28 @@ public class KitSelection {
         int random = new Random().nextInt(3);
 
         if(random == 0){
-            Kits.kit.put(player, "Sorcier");
-            player.sendMessage("§eKit aléatoire sélectionné : "+Kits.kit.get(player));
+            Main.kit.put(player, "Sorcier");
+            player.sendMessage("§eKit aléatoire sélectionné : "+Main.kit.get(player));
             player.playSound(player.getLocation(), Sound.CLICK, 30, 2);
 
             KitSelection.kitInventory(player.getPlayer());
             LobbyScoreboard.Scoreboardd(player, "§aSorcier");
-            return;
         }
         else if(random == 1){
-            Kits.kit.put(player, "Guerrier");
-            player.sendMessage("§eKit aléatoire sélectionné : "+Kits.kit.get(player));
+            Main.kit.put(player, "Guerrier");
+            player.sendMessage("§eKit aléatoire sélectionné : "+Main.kit.get(player));
             player.playSound(player.getLocation(), Sound.CLICK, 30, 2);
 
             KitSelection.kitInventory(player);
             LobbyScoreboard.Scoreboardd(player, "§aGuerrier");
-            return;
         }
         else if(random == 2){
-            Kits.kit.put(player, "Archer");
-            player.sendMessage("§eKit aléatoire sélectionné : "+Kits.kit.get(player));
+            Main.kit.put(player, "Archer");
+            player.sendMessage("§eKit aléatoire sélectionné : "+Main.kit.get(player));
             player.playSound(player.getLocation(), Sound.CLICK, 30, 2);
 
             KitSelection.kitInventory(player);
             LobbyScoreboard.Scoreboardd(player, "§aArcher");
-            return;
         }
 
     }
