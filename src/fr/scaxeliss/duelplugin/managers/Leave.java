1e762a831cf4e932f5519db62a209fd7210f076f
ScAxeliss <91017997+Axeliss@users.noreply.github.com>
2021-12-01 17:48:08 +0100

    [add] leave message when a player leave

--- src/fr/scaxeliss/duelplugin/managers/Leave.java
+++ src/fr/scaxeliss/duelplugin/managers/Leave.java
@@ -2,6 +2,7 @@ package fr.scaxeliss.duelplugin.managers;
 
 import fr.scaxeliss.duelplugin.Main;
 import fr.scaxeliss.duelplugin.scoreboard.LobbyScoreboard;
+import fr.scaxeliss.duelplugin.scoreboard.UpdateScoreboard;
 import fr.scaxeliss.duelplugin.timers.GameTimer;
 import fr.scaxeliss.duelplugin.timers.StartingTimer;
 import org.bukkit.Bukkit;
@@ -19,7 +20,24 @@ public class Leave implements Listener {
 
         int i = Bukkit.getOnlinePlayers().size() - 1;
 
+        e.setQuitMessage(null);
+
+        if(RegisterManager.starting){
+
+            UpdateScoreboard.updateLobbyScoreboard();
+
+            RegisterManager.starting = false;
+            StartingTimer.resetCountdown();
+
+            Bukkit.broadcastMessage("§7" + player.getName() + "§e vient de quitter la partie §e(§7" + i + "§e/§72§e)");
+            Bukkit.broadcastMessage("§cIl n'y a plus suffisamment de joueur, début annulé !");
+
+            return;
+        }
+
         if (RegisterManager.pvp) {
+
+            Bukkit.broadcastMessage("§7"+player.getName()+"§e vient d'abandonner la partie en quittant §e(§7" + i + "§e/§72§e)");
 
             StartingTimer.resetCountdown();
             RegisterManager.pvp = false;
@@ -34,12 +52,10 @@ public class Leave implements Listener {
 
                 Main.kit.replace(player1, "Aucun");
 
-                e.setQuitMessage("§7"+player.getName()+"§e vient d'abandonner la partie en quittant §e(§7" + i + "§e/§72§e)");
-
                 LobbyScoreboard.Scoreboardd(player1, Main.kit.get(player1));
                 player1.getInventory().setItem(0, ItemBuilder.itemWithLore(Material.COMPASS, "§aKits", "", "§eClique pour ouvrir le menu des kits !"));
             }
 
-        } else e.setQuitMessage("§7" + player.getName() + "§e vient de quitter la partie §e(§7" + i + "§e/§72§e)");
+        } else Bukkit.broadcastMessage("§7" + player.getName() + "§e vient de quitter la partie §e(§7" + i + "§e/§72§e)");
     }
 }
